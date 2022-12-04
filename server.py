06c96b10d1123b672d258cd331fa97d648a13e5f
kyle <kyle@banana.dev>
2022-12-04 12:33:02 +0000

    make headless.

--- server.py
+++ server.py
@@ -12,13 +12,13 @@ APP = flask.Flask(__name__)
 PLAY = sync_playwright().start()
 BROWSER = PLAY.chromium.launch_persistent_context(
     user_data_dir="/tmp/playwright",
-    headless=False,
+    headless=True,
 )
 PAGE = BROWSER.new_page()
 
 def get_input_box():
     """Get the child textarea of `PromptTextarea__TextareaWrapper`"""
-    return PAGE.query_selector("textarea")
+    return PAGE.query_selector('.PromptTextarea__TextareaWrapper-sc-4snkpf-0 textarea')
 
 def is_logged_in():
     # See if we have a textarea with data-id="root"
@@ -27,8 +27,9 @@ def is_logged_in():
 def send_message(message):
     # Send the message
     box = get_input_box()
-    box.click()
-    box.fill(message)
+    # print css info about box
+    print(str(box.inner_html))
+    box.fill(message) # click seems to freeze up on headless, so just fill textarea by force
     box.press("Enter")
 
 def get_last_message():
@@ -49,13 +50,37 @@ def chat():
 
 def start_browser():
     PAGE.goto("https://chat.openai.com/")
+
     if not is_logged_in():
-        print("Please log in to OpenAI Chat")
-        print("Press enter when you're done")
-        input()
+        print(" attempting to login to openai")
+        # Find the "Log in" button on the page
+        login_button = PAGE.get_by_role("button", name="Log in")
+
+        # Click the button
+        login_button.click()
+
+        print("Clicked login button")
+
+        # fill in login form
+        # Enter your login information
+        email_field = PAGE.locator('label')
+        continue_button = PAGE.locator("button[name=\"action\"]")
+
+        email_field.fill("kyle@banana.dev")
+        continue_button.click()
+
+        print("filled in email")
+
+        # Submit the login form
+        password_field = PAGE.locator("label")
+        password_field.fill("bananabanana12")
+        continue_button = PAGE.locator("button[name=\"action\"]")
+        continue_button.click()
+
     else:
         print("Logged in")
         APP.run(port=5001, threaded=False)
 
 if __name__ == "__main__":
     start_browser()
+
